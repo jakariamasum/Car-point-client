62c931eade91bb7d4343ac9abec36e66e4070301
jakariamasum <jmphy698@gmail.com>
2024-05-30 19:12:22 +0600

    user activity add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "./About";
 import Banner from "./Banner";
 import Newsletter from "./NewsLetter";
+import UserActivity from "./UserActivity";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <UserActivity />
       <About />
       <Newsletter />
     </div>
